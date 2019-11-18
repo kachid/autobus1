5e8efaa7096c8af2d806c0b722b515b46788f666
Mikhail Kakhidze <kachid76@gmail.com>
2019-11-18 23:34:52 +0500

    Add pages

---
+++ src/views/Tasks.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-title>Задачи</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items>
+      <v-btn text>Все задачи</v-btn>
+      <v-btn text>Мои задачи</v-btn>
+      <v-btn text>Задачи от меня</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'tasks',
+  data: () => ({
+    selection: [],
+    items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+  })
+}
+</script>
